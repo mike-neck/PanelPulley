0a6704f3da2c0756fd7f3a3c266079908dc593f8
mike-neck <jkrt3333@gmail.com>
2024-04-22 08:47:07 +0900

    Add resizing tests to PanelPulleyTest
    The commit introduces several tests to test the resizing functionality within the PanelPulleyTest module. The created tests validate various scenarios including handling situations where any or none of the options are set to -1.

--- Tests/PanelPulleyTest/ResizeTest.swift
+++ Tests/PanelPulleyTest/ResizeTest.swift
@@ -20,3 +20,86 @@ class ResizeEnvTest: XCTestCase {
     )
   }
 }
+
+class ResizeTest: XCTestCase {
+
+  func createValuedResize(
+    width: Int = 100,
+    height: Int = 100,
+    xAxis: Int = 100,
+    yAxis: Int = 100
+  ) -> Resize {
+    var resize = Resize()
+    resize.width = width
+    resize.height = height
+    resize.xAxis = xAxis
+    resize.yAxis = yAxis
+    return resize
+  }
+
+  enum ResizeOption {
+    case width(_ value: Int)
+    case height(_ value: Int)
+    case xAxis(_ value: Int)
+    case yAxis(_ value: Int)
+
+    case nothing
+
+  }
+
+  func createNoOptionResize(exceptFor option: ResizeOption = .nothing) -> Resize {
+    var resize = createValuedResize(width: -1, height: -1, xAxis: -1, yAxis: -1)
+    switch option {
+    case .width(let width):
+      resize.width = width
+    case .height(let height):
+      resize.height = height
+    case .xAxis(let xAxis):
+      resize.xAxis = xAxis
+    case .yAxis(let yAxis):
+      resize.yAxis = yAxis
+    case .nothing: break
+    }
+    return resize
+  }
+
+  func testValidate_WithNotAllOptionBeingMinus1_ThrowsNoError() throws {
+    let resizes: [Resize] = [
+      createValuedResize(width: -1),
+      createValuedResize(height: -1),
+      createValuedResize(xAxis: -1),
+      createValuedResize(yAxis: -1),
+    ]
+    for r in resizes {
+      var resize = r
+      XCTAssertNoThrow(
+        try resize.validate(),
+        "validate, with not all option being -1, throws no error"
+      )
+    }
+  }
+
+  func testValidate_WithAnyHavingOption_ThrowsNoError() throws {
+    let resizes: [Resize] = [
+      createNoOptionResize(exceptFor: .width(400)),
+      createNoOptionResize(exceptFor: .height(400)),
+      createNoOptionResize(exceptFor: .xAxis(400)),
+      createNoOptionResize(exceptFor: .yAxis(400)),
+    ]
+    for r in resizes {
+      var resize = r
+      XCTAssertNoThrow(
+        try resize.validate(),
+        "validate, with not all option being -1, throws no error"
+      )
+    }
+  }
+
+  func testValidate_WithAllOptionBeingMinus1_ThrowsError() {
+    var resize = createValuedResize(width: -1, height: -1, xAxis: -1, yAxis: -1)
+    XCTAssertThrowsError(
+      try resize.validate(),
+      "validate, with all option being -1, throws error"
+    )
+  }
+}
